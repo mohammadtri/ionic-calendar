c5c37d71b4987329aa1bee8183abb119512f7e4c
mohammadtri <45417939+mohammadtri@users.noreply.github.com>
2019-05-28 12:38:42 +0700

    Update month-name.ts

--- src/calendar/pipes/month-name.ts
+++ src/calendar/pipes/month-name.ts
@@ -1,12 +1,29 @@
+// import {Pipe} from '@angular/core';
+// import * as moment from 'moment';
+
+// @Pipe({
+//   name: 'monthName'
+// })
+// export class monthName {
+//   transform(value, args) {
+//     var lang = moment().locale(args);
+//     return lang.localeData().months(moment([2012, value - 1]));
+//   }
+// }
+
 import {Pipe} from '@angular/core';
-import * as moment from 'moment';
 
 @Pipe({
   name: 'monthName'
 })
 export class monthName {
+  private lang: string; // (es or en)
   transform(value, args) {
-    var lang = moment().locale(args);
-    return lang.localeData().months(moment([2012, value - 1]));
+    if (args === 'es') { this.lang = 'es'; }
+    let monthNames = [ 'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December' ];
+    if (this.lang === 'es') {
+      monthNames = [ 'Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre' ];
+    }
+    return monthNames[value - 1];
   }
 }
